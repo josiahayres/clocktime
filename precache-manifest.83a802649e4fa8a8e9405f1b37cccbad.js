e0575c1b0d505c54bb2b2818a6f3ac329d081487
Josiah Ayres <jayres@nz1.ibm.com>
2020-06-07 18:21:10 +1200

    Updates

---
+++ precache-manifest.83a802649e4fa8a8e9405f1b37cccbad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d75199215ac4c67c657e9dff8926a07",
+    "url": "/clocktime/index.html"
+  },
+  {
+    "revision": "3924f32d4036706b523f",
+    "url": "/clocktime/static/css/main.146d5575.chunk.css"
+  },
+  {
+    "revision": "f865266e2494b0cc85ce",
+    "url": "/clocktime/static/js/2.c94015db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clocktime/static/js/2.c94015db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3924f32d4036706b523f",
+    "url": "/clocktime/static/js/main.f9745e35.chunk.js"
+  },
+  {
+    "revision": "18dd8a6e0ee6dba5fad5",
+    "url": "/clocktime/static/js/runtime-main.2c5704bd.js"
+  }
+]);
