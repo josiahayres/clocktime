8b5e9f25efa0f50e43c4b06af3d0962a32872ff7
josiahayres <josiahayres@users.noreply.github.com>
2020-06-20 02:50:37 +0000

    Deploying to gh-pages from  @ 9f16e927ba652d9ddf28032ac6a4e9a7fd9580e7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clocktime/precache-manifest.185af7c713c7e2087e6fc2d70a9184cc.js"
+  "/clocktime/precache-manifest.30f99dfb50b8961882562c79426e00b2.js"
 );
 
 self.addEventListener('message', (event) => {
