1c27f3c3086984db71b79f277e4184076d9ddc7f
Josiah Ayres <jayres@nz1.ibm.com>
2020-06-21 15:33:50 +1200

    Added whole lot of options

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Digit from './components/Digit/Digit';
 import Conditional from './components/Conditional/Conditional';
+import Toggle from "./components/Toggle/Toggle";
 import pkg from '../package.json';
 
 import './App.css';
@@ -13,27 +14,13 @@ function App() {
   const [minute2, setMinute2] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [seconds2, setSeconds2] = useState(0);
-  const [is24hr] = useState(false);
-  const [showSeconds] = useState(false);
 
-  useEffect(() => {
+  const [optionsVisible, setOptionsVisible] = useState(false);
 
-    // 12 hr time
-    const timer = (is24hr || time.getHours() <= 12) ? time.getHours() : time.getHours() % 12;
-
-    const hourStr = (timer).toString().padStart(2, "0");
-    const minuteStr = (time.getMinutes().toString().padStart(2, 0));
-
-    const secondsStr = (time.getSeconds().toString().padStart(2, 0));
-
-    setHour(Number(hourStr[0]));
-    setHour2(Number(hourStr[1]));
-    setMinute(Number(minuteStr[0]));
-    setMinute2(Number(minuteStr[1]));
-
-    setSeconds(Number(secondsStr[0]));
-    setSeconds2(Number(secondsStr[1]));
-  }, [time, is24hr])
+  /** Options */
+  const [is24hr, setIs24hr] = useState(false);
+  const [showSeconds, setShowSeconds] = useState(false);
+  const [showDivider, setShowDivider] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => setTime(new Date()), 1000);
@@ -42,26 +29,73 @@ function App() {
     }
   });
 
-  return (
-    <div className="app">
-      <div className="time">
-        <Digit number={hour} />
-        <Digit number={hour2} />
+  useEffect(() => {
+    // 12 hr time
+    const timer = (is24hr || time.getHours() <= 12) ? time.getHours() : time.getHours() % 12;
+
+    const hourStr = (timer).toString().padStart(2, "0");
+    const minuteStr = (time.getMinutes().toString().padStart(2, 0));
+    const secondsStr = (time.getSeconds().toString().padStart(2, 0));
+
+    setHour(Number(hourStr[0]));
+    setHour2(Number(hourStr[1]));
+    setMinute(Number(minuteStr[0]));
+    setMinute2(Number(minuteStr[1]));
+    setSeconds(Number(secondsStr[0]));
+    setSeconds2(Number(secondsStr[1]));
+  }, [time, is24hr]);
+
+  const optionsOverlay = <div className="overlay" >
+    <div className="optionsContainer" onClick={(e) => e.stopPropagation()}>
+      <h2>Clocktime</h2>
+      <div className="options">
+        <h3>Options</h3>
+        <Toggle
+          label="Seconds"
+          checked={showSeconds}
+          onClick={() => setShowSeconds(!showSeconds)} />
+        <Toggle
+          label="24 hours"
+          checked={is24hr}
+          onClick={() => setIs24hr(!is24hr)} />
+        <Toggle
+          label="Divider"
+          checked={showDivider}
+          onClick={() => setShowDivider(!showDivider)} />
+        <h3>Background</h3>
+        <p>Coming soon...</p>
+        <h3>Save preferences</h3>
+        <p>Coming soon...</p>
       </div>
-      <div className="divider">:</div>
-      <div className="time">
-        <Digit number={minute} />
-        <Digit number={minute2} />
-      </div>
-      <Conditional when={showSeconds}>
-        <div className="divider">:</div>
-        <div className="time">
-          <Digit number={seconds} />
-          <Digit number={seconds2} />
-        </div>
-      </Conditional>
-      <span>{`${pkg.name} (${pkg.version})`}</span>
+      <button className="closeButton" onClick={() => setOptionsVisible(false)}>Close</button>
     </div>
+  </div>;
+
+  return (
+    <>
+      <Conditional when={optionsVisible}>
+        {optionsOverlay}
+      </Conditional>
+      <div className="app">
+        <div className="time">
+          <Digit number={hour} />
+          <Digit number={hour2} />
+        </div>
+        <Conditional when={showDivider}><div className="divider">:</div></Conditional>
+        <div className="time">
+          <Digit number={minute} />
+          <Digit number={minute2} />
+        </div>
+        <Conditional when={showSeconds}>
+          <Conditional when={showDivider}><div className="divider">:</div></Conditional>
+          <div className="time">
+            <Digit number={seconds} />
+            <Digit number={seconds2} />
+          </div>
+        </Conditional>
+        <span><span className="optionsBtn" onClick={() => setOptionsVisible(true)}>options</span>  | {`${pkg.name} (${pkg.version})`}</span>
+      </div>
+    </>
   );
 }
 
