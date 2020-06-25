acd7207342d9b81a66b714c215f684c6fc995d9d
Josiah Ayres <jayres@nz1.ibm.com>
2020-06-25 21:18:37 +1200

    Fix warning

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Radio.css";
 
 const Radio = (props) => {
-    const { onClick, size = "size", name = "id", selected = "" } = props;
+    const { onClick, size = "size", selected = "" } = props;
     return (
 
         <li id={size} onClick={() => onClick(size)}>
-            <input type="radio" name="clocksize" value={size} checked={selected == size} />
+            <input type="radio" name="clocksize" value={size} checked={selected === size} />
             <label htmlFor={size}>{size}</label>
             <div className="check"><div className="inside"></div></div>
         </li >
