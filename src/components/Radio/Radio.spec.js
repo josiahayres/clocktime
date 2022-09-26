1d39473f0379e821829c7b4a8101aef15a42cb2d
Josiah Ayres <josiah@lend.com.au>
2022-09-26 17:46:20 +1300

    Minor: fix tests

--- src/components/Radio/Radio.spec.js
+++ src/components/Radio/Radio.spec.js
@@ -4,32 +4,32 @@ import userEvent from "@testing-library/user-event";
 import RadioGroup from "./RadioGroup";
 import Radio from "./Radio";
 
-test("click", () => {
-	let clockSize = "tiny";
-	const handleChange = jest.fn((newId) => (clockSize = newId));
+test("click", async () => {
+  let clockSize = "tiny";
+  const handleChange = jest.fn((newId) => (clockSize = newId));
 
-	render(
-		<RadioGroup>
-			<label htmlFor="checkbox">Check</label>
-			<Radio
-				value="tiny"
-				name="clocksize"
-				onClick={handleChange}
-				selected={clockSize}
-			/>
-			<Radio
-				value="small"
-				name="clocksize"
-				onClick={handleChange}
-				selected={clockSize}
-			/>
-		</RadioGroup>
-	);
+  render(
+    <RadioGroup>
+      <label htmlFor="checkbox">Check</label>
+      <Radio
+        value="tiny"
+        name="clocksize"
+        onClick={handleChange}
+        selected={clockSize}
+      />
+      <Radio
+        value="small"
+        name="clocksize"
+        onClick={handleChange}
+        selected={clockSize}
+      />
+    </RadioGroup>
+  );
 
-	expect(screen.getAllByRole("radio")).toHaveLength(2);
-	expect(screen.getAllByRole("radio")[0]).toHaveAttribute("checked", "");
-	expect(screen.getAllByRole("radio")[1]).not.toHaveAttribute("checked", "");
-	userEvent.click(screen.getByLabelText("small"));
-	expect(handleChange.mock.calls.length).toBe(1);
-	expect(clockSize).toBe("small");
+  expect(screen.getAllByRole("radio")).toHaveLength(2);
+  expect(screen.getAllByRole("radio")[0]).toHaveAttribute("checked", "");
+  expect(screen.getAllByRole("radio")[1]).not.toHaveAttribute("checked", "");
+  await userEvent.click(screen.getByText("small"));
+  expect(handleChange.mock.calls.length).toBe(1);
+  expect(clockSize).toBe("small");
 });
