4a79fe1b194c1f7e6f02f3cf15cc9318fd3aed5e
Josiah Ayres <josiah@lend.com.au>
2022-09-26 17:30:28 +1300

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { SnackbarProvider } from "notistack";
+import {
+  ThemeProvider,
+  StyledEngineProvider,
+  createTheme,
+} from "@mui/material/styles";
+
 import Clock from "./pages/Clock.js";
 
+const theme = createTheme();
+
 function App(props) {
-	return (
-		<SnackbarProvider>
-			<Clock />
-		</SnackbarProvider>
-	);
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <SnackbarProvider>
+          <Clock />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
 export default App;
